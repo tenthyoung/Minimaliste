0de9394bfaf6a9161ba2d4737fcc64efa91c236c
tenthyoung <tenthyoung@gmail.com>
2019-07-25 20:58:35 -0700

    new starter files from bookSearch homework solution

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080;
+const express = require("express");
+
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-app.get('/', (req,res) => {
-    res.send('hi');
-});
+// Add routes, both API and view
+app.use(routes);
 
-app.listen(PORT, function() {
-   console.log('App listening on http://localhost:' + PORT);
-});
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://user1:password1@ds125871.mlab.com:25871/heroku_0xn0jnk7",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  }
+);
+
+// Start the API server
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
