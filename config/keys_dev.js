c4171cfa1183a863bea7e77e164906c939e634d4
TenthYoung <tenthyoung@gmail.com>
2019-08-08 15:26:56 -0700

    deploying to heroku

---
+++ config/keys_dev.js
@@ -0,0 +1,6 @@
+// when you run locally, it'll use these keys
+
+module.exports = {
+    stripePublishableKey: 'pk_test_3MNOlrzuvTGzFOj1elrOJOJn00W6kPUyzS',
+    stripeSecretKey: 'sk_test_wBuUG5YceTDCmb9reyIrONTf007dw8YQiG'
+}
